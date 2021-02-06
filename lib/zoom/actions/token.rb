1f7f3e7d9643813984fd4aa1f3a2fbf411a8ae2f
Motoyasu Yamada <motoyasu.yamada@gmail.com>
2021-02-06 21:33:04 +0900

    Bugfix

--- lib/zoom/actions/token.rb
+++ lib/zoom/actions/token.rb
@@ -5,7 +5,7 @@ module Zoom
     module Token
       def access_tokens(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
-        options.require(%i[client_id　auth_code redirect_uri])
+        options.require(%i[auth_code redirect_uri])
         response = self.class.post("/oauth/token?grant_type=authorization_code&client_id=#{Zoom.configuration.client_id}&code=#{options[:auth_code]}&redirect_uri=#{options[:redirect_uri]}", headers: oauth_request_headers, base_uri: 'https://zoom.us/')
         Utils.parse_response(response)
       end
